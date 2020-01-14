18536949eb0a5c0922fecd3fc4b0398d4d16dabb
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 18:04:27 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,14 @@
-print("hello")
-print("world)
+if x < y: 
+  print('x is less than y')
+elif x > y:
+  print('x is greater than y')
+else:
+  print('x and y are equal')
+  
+  
+if choice == 'a':
+   draw_a()
+elif choice == 'b':
+   draw_b()
+elif choice == 'c':
+   draw_c()
